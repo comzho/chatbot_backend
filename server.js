8d2d3cffedd5b1c2c9ac673d980ba8c1eb9c506c
comzho <Japocjapoc58@gmail.com>
2025-03-05 09:30:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
+
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
